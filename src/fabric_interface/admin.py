deb630fb38bd0e77af747fd3267e7f964eccfc82
Hedde van der Heide <me@heddevanderheide.nl>
2014-03-16 17:29:38 +0100

    added admin, todo django-guardian

---
+++ src/fabric_interface/admin.py
@@ -0,0 +1,108 @@
+__author__ = 'heddevanderheide'
+
+# Django specific
+from django import forms
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Group
+from django.utils.translation import ugettext_lazy as _
+
+# App specific
+from fabric_interface.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email',)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = get_user_model()
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    # change_list_template = "admin/change_list_filter_sidebar.html"
+
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'get_full_name_display', 'is_active', 'is_admin')
+    list_filter = ('is_active', 'is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': [
+            ('first_name',),
+            ('family_name_prefix', 'family_name')
+        ]}),
+        ('Permissions', {'fields': ('is_active', 'is_admin',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+    readonly_fields = ('last_login', 'date_joined',)
+
+    def get_queryset(self, request):
+        return super(UserAdmin, self).get_queryset(request).filter(
+            content_type__name='user'
+        )
+
+    def get_full_name_display(self, obj):
+        return obj.get_full_name()
+    get_full_name_display.short_description = _(u"Name")
+
+
+# Now register the new UserAdmin...
+admin.site.register(User, UserAdmin)
+# ... and, since we're not using Django's builtin permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
