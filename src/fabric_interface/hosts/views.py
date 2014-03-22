a858b4a27bb54351cd7b1e8b1918d681cd949a7a
Hedde van der Heide <me@heddevanderheide.nl>
2014-03-22 14:22:15 +0100

    style update

--- src/fabric_interface/hosts/views.py
+++ src/fabric_interface/hosts/views.py
@@ -3,13 +3,24 @@ __author__ = 'heddevanderheide'
 # Django specific
 from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import (
-    CreateView, UpdateView
+    DetailView, CreateView, UpdateView, DeleteView
 )
 # App specific
 from fabric_interface.hosts.models import Host
 from viewsets import ModelViewSet, SLUG
+
+
+class HostDetailView(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super(HostDetailView, self).get_context_data(**kwargs)
+        context.update({
+            'title': _(u"View"),
+            'action': 'view'
+        })
+        return context
 
 
 class HostCreateView(CreateView):
@@ -54,11 +65,38 @@ class HostUpdateView(UpdateView):
         return reverse('host_detail', kwargs={'slug': self.object.slug})
 
 
+class HostDeleteView(DeleteView):
+    success_url = reverse_lazy('host_index')
+
+    def get_context_data(self, **kwargs):
+        context = super(HostDeleteView, self).get_context_data(**kwargs)
+        context.update({
+            'title': _(u"Delete"),
+            'action': 'delete'
+        })
+        return context
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+
+        messages.add_message(
+            self.request, messages.SUCCESS, _(u"Deleted {model} '{slug}' succesfully.".format(
+                model=self.model._meta.verbose_name,
+                slug=self.object.slug
+            ))
+        )
+        self.object.delete()
+        return HttpResponseRedirect(success_url)
+
+
 class HostViewSet(ModelViewSet):
     model = Host
     id_pattern = SLUG
 
     def __init__(self, *args, **kwargs):
+        self.views[b'detail_view']['view'] = HostDetailView
         self.views[b'create_view']['view'] = HostCreateView
         self.views[b'update_view']['view'] = HostUpdateView
+        self.views[b'delete_view']['view'] = HostDeleteView
         super(HostViewSet, self).__init__(*args, **kwargs)
