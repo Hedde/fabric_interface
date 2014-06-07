400ffc87e1020888a70b9b41c3954b3b8b774ce4
Hedde <me@heddevanderheide.nl>
2014-06-07 10:08:29 +0200

    cleanup

--- src/main/urls.py
+++ src/main/urls.py
@@ -2,9 +2,7 @@ __author__ = 'heddevanderheide'
 
 # Django specific
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
-admin.autodiscover()
 
 urlpatterns = patterns('',
     url('', include('fabric_interface.urls'))
